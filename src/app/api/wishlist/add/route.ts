869c9befa4dfd1c188ce11ef8ef81e2dd4173dfc
ashishrajz <ASHISH.RAJ.MEC24@ITBHU.AC.IN>
2025-07-03 12:55:39 +0530

    Update route.ts

--- src/app/api/wishlist/add/route.ts
+++ src/app/api/wishlist/add/route.ts
@@ -5,13 +5,13 @@ import { NextRequest, NextResponse } from "next/server";
 import { addToWishlist } from "@/actions/wishlist.actions";
 import { getDbUserId } from "@/actions/user.actions";
 
-
-
 export async function POST(req: NextRequest) {
   const { userId } = await auth();
   const { courseId } = await req.json();
   if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
   const dbUserId = await getDbUserId(userId);
-  const wishlist = await addToWishlist(dbUserId, courseId);
+  const wishlist = await addToWishlist(dbUserId as string, courseId); // ✅ fix
+
   return NextResponse.json(wishlist);
 }
