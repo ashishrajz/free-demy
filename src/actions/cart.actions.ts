aabf054e00dc267e41579c7e40925bb28d9c06c3
ashishrajz <ASHISH.RAJ.MEC24@ITBHU.AC.IN>
2025-07-03 08:24:34 +0530

    Update cart.actions.ts

--- src/actions/cart.actions.ts
+++ src/actions/cart.actions.ts
@@ -1,17 +1,24 @@
 // src/actions/cart.ts
+import mongoose from "mongoose";
 import Course from "@/lib/models/course.model";
 import User from "@/lib/models/user.model";
 import { connectDB } from "@/lib/db";
 
 export const addToCart = async (userId: string, courseId: string) => {
   await connectDB();
+
   const user = await User.findById(userId);
   if (!user) throw new Error("User not found");
 
-  if (!user.cart.includes(courseId)) {
-    user.cart.push(courseId);
+  const courseObjectId = new mongoose.Types.ObjectId(courseId);
+
+  const isInCart = user.cart.some((id) => id.equals(courseObjectId));
+
+  if (!isInCart) {
+    user.cart.push(courseObjectId);
     await user.save();
   }
+
   return user.cart;
 };
 
